216b9b44e97006f17129000d3491fd6a55e9bbf1
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2021-07-19 01:43:01 -0700

    Downloaded setup files

---
+++ Lyrical/src/main/java/com/personalProject/Lyrical/LyricalApplication.java
@@ -0,0 +1,13 @@
+package com.personalProject.Lyrical;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LyricalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LyricalApplication.class, args);
+	}
+
+}
---
+++ Lyrical/src/test/java/com/personalProject/Lyrical/LyricalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalProject.Lyrical;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LyricalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
