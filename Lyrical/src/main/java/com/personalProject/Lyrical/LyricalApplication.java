45d3e358499779c16431cdf741a98316814f3dd3
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2021-07-20 01:46:42 -0700

    Added song controller and placeholder song html page

--- Lyrical/src/main/java/com/personalProject/Lyrical/LyricalApplication.java
+++ Lyrical/src/main/java/com/personalProject/Lyrical/LyricalApplication.java
@@ -9,5 +9,4 @@ public class LyricalApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(LyricalApplication.class, args);
 	}
-
 }
---
+++ Lyrical/src/main/java/com/personalProject/Lyrical/Song/SongController.java
@@ -0,0 +1,18 @@
+package com.personalProject.Lyrical.Song;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class SongController {
+
+//    @GetMapping("/song")
+//    public String getSong(@RequestParam(name = "name", required = false, defaultValue = "Nonstop") String name, Model model) {
+//        model.addAttribute("name", name);
+//        return "song";
+//    }
+}
