dcedff5b77aa89b84ba0b142ff6b2845726ac5a7
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2021-08-24 02:42:32 -0700

    added configuration file. Changed application.properties to application.yml. Updated dependencies. Updated templates to reflect login functionality. Unable to re-enter login credentials once logged in.

--- Lyrical/src/main/java/com/personalProject/Lyrical/Controllers/HomeController.java
+++ Lyrical/src/main/java/com/personalProject/Lyrical/Controllers/HomeController.java
@@ -1,14 +1,14 @@
 package com.personalProject.Lyrical.Controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class HomeController {
 
     @GetMapping("/")
-    public String getHomePage() {
+    public String getHomePage(Authentication authentication) {
         return "home";
     }
 }
