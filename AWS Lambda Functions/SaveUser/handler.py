38a3ca020075ba2506ea0fa38294fbd958d554c6
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2021-08-26 19:11:55 -0700

    changed db table 'Users' and replaced 'id' with 'username'. Updated saveUser lambda function to reflect this

--- AWS Lambda Functions/SaveUser/handler.py
+++ AWS Lambda Functions/SaveUser/handler.py
@@ -1,1 +1,25 @@
-handler.py
+import os
+import pymysql
+
+# Configuration Values
+endpoint = os.environ['RDS_ENDPOINT']
+username = os.environ['RDS_USERNAME']
+password = os.environ['RDS_PASSWORD']
+database_name = os.environ['RDS_DATABASE']
+
+# Connection
+connection = pymysql.connect(host=endpoint, user=username, passwd=password, db=database_name)
+
+def saveUsers_handler(event, context):
+    
+    email = event['request']['userAttributes']['email']
+    username = event['userName']
+    sql_query = "INSERT INTO Users (username, email, name, address, birthdate) VALUES (%s, %s, null, null, null)"
+
+    cursor = connection.cursor()
+    cursor.execute(sql_query, (username, email))
+    
+    connection.commit()
+
+
+    return event
