3be53391100ae67e5b06ca427ee3188dbd85e71f
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2021-08-27 16:38:05 -0700

    Removed address and birthdate from user model

--- Lyrical/src/main/java/com/personalProject/Lyrical/Models/User.java
+++ Lyrical/src/main/java/com/personalProject/Lyrical/Models/User.java
@@ -5,17 +5,13 @@ public class User {
     private String username;
     private String email;
     private String name;
-    private String address;
-    private String birthdate;
 
     public User() {}
 
-    public User(String username, String email, String name, String address, String birthdate) {
+    public User(String username, String email, String name) {
         this.username = username;
         this.email = email;
         this.name = name;
-        this.address = address;
-        this.birthdate = birthdate;
     }
 
     public String getUsername() {
@@ -42,30 +38,12 @@ public class User {
         this.name = name;
     }
 
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getBirthdate() {
-        return birthdate;
-    }
-
-    public void setBirthdate(String birthdate) {
-        this.birthdate = birthdate;
-    }
-
     @Override
     public String toString() {
         return "User{" +
                 "username=" + username +
                 ", email='" + email + '\'' +
                 ", name='" + name + '\'' +
-                ", address='" + address + '\'' +
-                ", birthdate='" + birthdate + '\'' +
                 '}';
     }
 }
