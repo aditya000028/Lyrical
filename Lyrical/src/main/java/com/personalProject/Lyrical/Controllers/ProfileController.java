b5d1115b004e74123e021dafb78943436fb94ae2
Aditya Gupta <aditya.gupta_chd@yahoo.ca>
2021-08-26 18:13:27 -0700

    updated profile page to view user info. Added profile service file for parsing result from api.

--- Lyrical/src/main/java/com/personalProject/Lyrical/Controllers/ProfileController.java
+++ Lyrical/src/main/java/com/personalProject/Lyrical/Controllers/ProfileController.java
@@ -4,9 +4,13 @@ import com.personalProject.Lyrical.Models.User;
 import com.personalProject.Lyrical.ServiceHelpers.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import java.io.IOException;
 
 @Controller
 public class ProfileController {
@@ -19,12 +23,18 @@ public class ProfileController {
     }
 
     @GetMapping("/profile")
-    public String getUserProfile(Authentication authentication, Model model) {
+    public String getUserProfile(Authentication authentication, Model model, @AuthenticationPrincipal OAuth2User oAuth2User) {
 
-        if (authentication != null) {
+        if (authentication != null && authentication.isAuthenticated()) {
 
-            // make a call from profile service helper to api to fetch user info from db
+            String username = oAuth2User.getAttribute("name");
 
+            try {
+               User user = profileService.getUserProfile(username);
+               model.addAttribute("userProfile", user);
+            } catch (IOException ioException) {
+                model.addAttribute("error", ioException.getMessage());
+            }
 
             return "profile";
         } else {
@@ -32,5 +42,11 @@ public class ProfileController {
             // Or direct straight to login
             return "home";
         }
+    }
+
+    @PostMapping("/profileUpdate")
+    public String updateUserProfile() {
+        // perform update
+        return "profile";
     }
 }
--- Lyrical/src/main/java/com/personalProject/Lyrical/ServiceHelpers/ProfileService.java
+++ Lyrical/src/main/java/com/personalProject/Lyrical/ServiceHelpers/ProfileService.java
@@ -1,9 +1,16 @@
 package com.personalProject.Lyrical.ServiceHelpers;
 
+import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import com.personalProject.Lyrical.Models.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 @Service
 public class ProfileService {
@@ -11,16 +18,31 @@ public class ProfileService {
     @Value("${AWS_PROFILE_API}")
     private String AWS_PROFILE_API;
 
-//    public User getUserProfile(String email) {
-//        return parseResponse(makeAPICall(email));
-//    }
-//
-//    private JsonObject makeAPICall(String email) {
-//
-//    }
-//
-//    private User parseResponse(JsonObject obj) {
-//
-//    }
+    public User getUserProfile(String username) throws IOException {
+        return parseResponse(makeAPICall(username));
+    }
+
+    private JsonObject makeAPICall(String username) throws IOException {
+        String request = AWS_PROFILE_API + username;
+
+        URL url = new URL(request);
+        HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+        JsonElement jsonElement = JsonParser.parseReader(new InputStreamReader(httpURLConnection.getInputStream()));
+        httpURLConnection.disconnect();
+
+        return jsonElement.getAsJsonObject();
+    }
+
+    private User parseResponse(JsonObject obj) {
+        User user = new User();
+
+        user.setId(obj.get("id").getAsLong());
+        user.setAddress(obj.get("address").getAsString());
+        user.setName(obj.get("name").getAsString());
+        user.setEmail(obj.get("email").getAsString());
+        user.setBirthdate(obj.get("birthdate").getAsString());
+
+        return user;
+    }
 
 }
